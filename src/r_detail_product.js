1169707ad3dd04329aedcff5c671ce4ba04d5c5f
Laura <laurasuzana91@gmail.com>
2018-06-11 16:42:11 +0930

    Front-End_JC5_LauraSuzana

--- src/r_detail_product.js
+++ src/r_detail_product.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'; 
 import { Link, Route } from 'react-router-dom'; 
  
-import Header from './c_home_header'; 
+import Header from './c_header'; 
 import Footer from './c_home_footer'; 
 
 class r_detail_product extends Component { 
@@ -10,6 +10,94 @@ class r_detail_product extends Component {
   <div>            
       <div>
         <Header/>         
+
+        <center>
+        <div className="panel panel-default" style={{width: '70%', marginTop:'70px'}}>
+          <div className="panel-heading"><center>Product Detail</center></div>
+          <div className="panel-body"><b> Healthy Meals Package</b></div><br />
+          <div id="myCarousel" className="carousel slide" data-ride="carousel">
+            {/* Indicators */}
+            <ol className="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to={0} className="active" />
+              <li data-target="#myCarousel" data-slide-to={1} />
+              <li data-target="#myCarousel" data-slide-to={2} />
+            </ol>
+            {/* Wrapper for slides */}
+            <div className="carousel-inner">
+              <div className="item active">
+                <img src={require("./images/hm1.png")} alt="Healthy Meal Example" />
+              </div>
+              <div className="item">
+                <img src={require("./images/hm2.png")} alt="Healthy Meal Example" />
+              </div>
+              <div className="item">
+                <img src={require("./images/hm3.png")} alt="Healthy Meal Example" />
+              </div>
+            </div>
+            {/* Left and right controls */}
+            <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+              <span className="glyphicon glyphicon-chevron-left" />
+              <span className="sr-only">Previous</span>
+            </a>
+            <a className="right carousel-control" href="#myCarousel" data-slide="next">
+              <span className="glyphicon glyphicon-chevron-right" />
+              <span className="sr-only">Next</span>
+            </a>
+          </div><br />
+          <center><h4 style={{textAlign: 'justify', lineHeight: 2, paddingLeft: 30, paddingRight: 30}}>
+              Adalah menu yang kami rancang untuk memenuhi gizi anda. Healthy Meals, tidak akan membuat berat badan anda turun, karena kami hanya mengganti menu harian anda dengan makanan yang memenuhi standar gizi sehari anda dan makanan yang lebih sehat bagi pencernaan dan seluruh tubuh. Disetiap sajian kami juga mengganti nasi putih dengan nasi merah yang lebih bergizi menurut para ahli.</h4></center><br />
+          <p>Tersedia beberapa pilihan paket untuk menu ini dari kami :
+          </p><br />
+          <table className="table">
+            <tbody><tr>
+                <td>
+                  Healthy Meals Package for Student
+                </td>
+                <td>
+                  Rp 15.000,00
+                </td>
+                <td>
+                  <button type="button" className="btn btn-info">Add to Cart</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Healthy Meals Package Regular
+                </td>
+                <td>
+                  Rp 50.000,00
+                </td>
+                <td>
+                  <button type="button" className="btn btn-info">Add to Cart</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>Healthy Meals Package for 1 Week,</p> 
+                  <p>3 times per day (7 days exclude Sunday)</p>
+                </td>
+                <td>
+                  Rp 950.000,00
+                </td>
+                <td>
+                  <button type="button" className="btn btn-info">Add to Cart</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>Healthy Meals Package for 2 Weeks,</p> 
+                  <p>3 times per day (14 days exclude Sunday)</p>
+                </td>
+                <td>
+                  Rp 1.850.000,00
+                </td>
+                <td>
+                  <button type="button" className="btn btn-info">Add to Cart</button>
+                </td>
+              </tr>
+            </tbody></table>
+        </div>
+      </center>
                
         <Footer/>         
       </div> 
--- src/r_login.js
+++ src/r_login.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'; 
 import { Link, Route } from 'react-router-dom'; 
  
-import Header from './c_home_header'; 
+import Header from './c_header'; 
 import Footer from './c_home_footer'; 
 
 class r_detail_product extends Component { 
@@ -10,7 +10,80 @@ class r_detail_product extends Component {
   <div>            
       <div>
         <Header/>         
-               
+
+        <div>
+        <div className="row">
+          <div className="col-md-4" />
+          <div className="col-md-4"> 
+            <h2 className="text-center">Pengunjung terdaftar? Silahkan Login disini. </h2>
+            <div className="panel panel-default">
+              <div className="panel-heading"><center>Please Login here</center></div>
+              <div className="panel-body">
+                <center>
+                  <table className="table">
+                    <tbody><tr>
+                        <td> Username : </td>
+                        <td> <input type="text" /> </td>
+                      </tr>
+                      <tr>
+                        <td> Password : </td>
+                        <td> <input type="password" /> </td>
+                      </tr>
+                    </tbody></table>
+                  <a className />
+                  <label className="checkbox-inline"><input type="checkbox" defaultValue />Please remember me</label><br /><br /><br />
+                  <button type="button" className="btn">Sign me in</button>
+                </center>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4" />
+        </div>
+        <center><p className="bg-info" style={{color: 'salmon'}}> If you never make an account before, you could register here </p></center><br /><br />      
+        <h2 className="text-center">Belum punya akun? Ayo daftar disini! </h2>
+        <div className="row">
+          <div className="col-md-2 col-xs-2 col-sm-2" /> 
+          <div className="col-md-8 col-xs-8 col-sm-8"> 
+            <center>
+              <div className="panel panel-default" style={{maxWidth: 500}}>
+                <div className="panel-heading"><center>Please Register here</center></div>
+                <div className="panel-body">
+                  <center>
+                    <table className="table" style={{maxWidth: 500}}>
+                      <tbody><tr>
+                          <td> Your Full Name : </td>
+                          <td> <input type="text" /> </td>
+                        </tr>
+                        <tr>
+                          <td> Username : </td>
+                          <td> <input type="text" /> </td>
+                        </tr>
+                        <tr>
+                          <td> Password : </td>
+                          <td> <input type="password" /> </td>
+                        </tr>
+                        <tr>
+                          <td> Re-Password : </td>
+                          <td> <input type="password" /> </td>
+                        </tr>
+                        <tr>
+                          <td> Telephone Number : </td>
+                          <td> <input type="text" /> </td>
+                        </tr>
+                        <tr>
+                          <td> Give us some address to send your package : </td>
+                          <td> <textarea className="form-control" rows={5} id="comment" defaultValue={""} /> </td>
+                        </tr>
+                      </tbody></table>
+                    <button type="button" className="btn">Register</button>
+                  </center>
+                </div>
+              </div>
+            </center></div>
+        </div> 
+        <div className="col-md-2 col-xs-2 col-sm-2"></div> 
+      </div>   
+
         <Footer/>         
       </div> 
 </div> 
